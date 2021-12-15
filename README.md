0ca184299b64614be9572eeddd2aad9861533685
zhang <z929527511@outlook.com>
2021-12-14 19:13:59 -0500

    modify repair.py to output plot, finish writing readme.md

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # wz2037_lab3
+
+```bash
+├── data 
+    └── cl
+        └── valid.h5 // this is clean validation data used to design the defense
+        └── test.h5  // this is clean test data used to evaluate the BadNet
+    └── bd
+        └── bd_valid.h5 // this is sunglasses poisoned validation data
+        └── bd_test.h5  // this is sunglasses poisoned test data
+├── models
+    └── bd_net.h5
+    └── bd_weights.h5
+    └── B_pi_net_0.1.h5
+    └── B_pi_net_0.04.h5
+    └── B_pi_net_0.02.h5
+├── architecture.py
+├── repair.py
+├── Geval.py // custom evaluation script
+└── eval.py // this is the evaluation script
+```
+
+## I. Dependencies
+   1. Python 3.6.9
+   2. Keras 2.3.1
+   3. Numpy 1.16.3
+   4. Matplotlib 2.2.2
+   5. H5py 2.9.0
+   6. TensorFlow-gpu 2.7.0
+   
+## II. Data
+   1. Download the validation and test datasets from [here](https://drive.google.com/drive/folders/1Rs68uH8Xqa4j6UxG53wzD0uyI8347dSq?usp=sharing) and store them under `data/` directory.
+   2. The dataset contains images from YouTube Aligned Face Dataset. We retrieve 1283 individuals and split into validation and test datasets.
+   3. bd_valid.h5 and bd_test.h5 contains validation and test images with sunglasses trigger respectively, that activates the backdoor for bd_net.h5. 
+
+## III. Evaluating the Backdoored Model
+   1. The DNN architecture used to train the face recognition model is the state-of-the-art DeepID network. 
+   2. To evaluate the backdoored model, execute `eval.py` by running:  
+      `python3 eval.py <clean validation data directory> <poisoned validation data directory> <model directory>`.
+      
+      E.g., `python3 eval.py data/cl/valid.h5 data/bd/bd_valid.h5 models/bd_net.h5`. This will output:
+      Clean Classification accuracy: 98.64 %
+      Attack Success Rate: 100 %
+
+## IV. Important Notes
+Please use only clean validation data (valid.h5) to design the pruning defense. And use test data (test.h5 and bd_test.h5) to evaluate the models. 
+
+## V. Repair Network
+  1. Repaired network for X={2%, 4%, 10%} is created by script *`repair.py`*, and output models are stored in *`models/B_pi_net_0.02.h5 models/B_pi_net_0.04.h5 models/B_pi_net_0.1.h5`* 
+  2. To evaluate the goodnet *G*, excute *`Geval.py`* by running:
+  
+     `python3 eval.py <clean validation data directory> <poisoned validation data directory> <B model directory> <B' model dirctory>`.
+     
+     For each repaired network, corresponding commands are:
+
+     `python3 eval.py data/test.h5 data/bd_test.h5 models/bd_net.h5 models/B_pi_net_0.02.h5`.
+
+     `python3 eval.py data/test.h5 data/bd_test.h5 models/bd_net.h5 models/B_pi_net_0.04.h5`.
+
+     `python3 eval.py data/test.h5 data/bd_test.h5 models/bd_net.h5 models/B_pi_net_0.1.h5`.
+
+  3. Evaluation results are:
+
+     - `B_pi_net_0.02.h5` : `Clean Classification accuracy`: 95.90023382696803
+`Attack Success Rate`: 100.0
+
+     - `B_pi_net_0.04.h5` : `Clean Classification accuracy`: 92.29150428682775
+`Attack Success Rate`: 99.98441153546376
+
+     - `B_pi_net_0.1.h5` : `Clean Classification accuracy`: 84.54403741231489
+`Attack Success Rate`: 77.20966484801247
+
+  4. Plot:
+    
+     - Clean accuracy: 
+     
+     ![](clean.png)
+
+     - Attack success rate:
+
+     ![](asr.png)
--- repair.py
+++ repair.py
@@ -3,6 +3,30 @@ import keras.backend as K
 import tensorflow as tf
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def data_loader(filepath):
+    data = h5py.File(filepath, 'r')
+    x_data = np.array(data['data'])
+    y_data = np.array(data['label'])
+    x_data = x_data.transpose((0, 2, 3, 1))
+
+    return x_data, y_data
+
+
+def G(bd_model, prune_model, bd_x_test, bd_y_test):
+    bd_label_p = np.argmax(bd_model.predict(bd_x_test), axis=1)
+    prune_label_p = np.argmax(prune_model.predict(bd_x_test), axis=1)
+    attack_label = np.max(bd_y_test) + 1
+    G_label = []
+
+    for i in range(len(bd_y_test)):
+        label = bd_label_p[i] if bd_label_p[i] == prune_label_p[i] else attack_label
+        G_label.append(label)
+
+    return np.array(G_label)
+
 
 def test():
     bd_model = keras.models.load_model('models/B_pi_net_0.1.h5')
@@ -13,10 +37,39 @@ def test():
     x_data = tf.convert_to_tensor(x_data, dtype=tf.float32)
     print(cal_accuracy(bd_model, x_data, y_data))
 
+
 def cal_accuracy(model, x_test, y_test):
     label_p = np.argmax(model.predict(x_test), axis=1)
     accuracy = np.mean(np.equal(label_p, y_test))
     return accuracy
+
+
+def cal_asr(bd_model, prune_model):
+    bd_x_test, bd_y_test = data_loader('data/bd_test.h5')
+
+    bd_label_p = G(bd_model, prune_model, bd_x_test, bd_y_test)
+    asr = np.mean(np.equal(bd_label_p, bd_y_test)) * 100
+    return asr
+
+
+def plot(clean_accuracy, asr):
+    fig, ax = plt.subplots()
+    ax.plot(clean_accuracy)
+
+    ax.set(xlabel='Prune channels', ylabel='Clean Classification accuracy',
+           title='Clean Classification accuracy over pruned channels')
+    ax.grid()
+
+    fig.savefig("clean.png")
+
+    fig, ax = plt.subplots()
+    ax.plot(asr)
+
+    ax.set(xlabel='Prune channels', ylabel='asr',
+           title='Attack Success Rate over pruned channels')
+    ax.grid()
+
+    fig.savefig("asr.png")
 
 
 def main(X):
@@ -40,6 +93,7 @@ def main(X):
     sorted_channel = np.argsort(sum_by_channel)
 
     weights = bd_model.get_weights()
+    B_pi_model.set_weights(weights)
 
     # names = [weight.name for layer in bd_model.layers for weight in layer.weights]
 
@@ -54,20 +108,31 @@ def main(X):
 
     clean_accuracy = cal_accuracy(bd_model, x_data, y_data)
     print('Clean Classification accuracy:', clean_accuracy)
+    asr = cal_asr(bd_model, B_pi_model)
     prune_accuracy = clean_accuracy
 
+    clean_accuracy_set = [prune_accuracy]
+    asr_set = [asr]
     output = 0
 
     for channel in sorted_channel:
         if sum_by_channel[channel] == 0:
+            weights[4][..., channel] = 0
             weights[5][channel] = 0
+            clean_accuracy_set.append(prune_accuracy)
+            asr_set.append(asr)
             continue
         weights[4][..., channel] = 0
         weights[5][channel] = 0
         B_pi_model.set_weights(weights)
         # print(B_pi_model.get_weights()[5])
         prune_accuracy = cal_accuracy(B_pi_model, x_data, y_data)
+        asr = cal_asr(bd_model, B_pi_model)
         print(prune_accuracy)
+
+        clean_accuracy_set.append(prune_accuracy)
+        asr_set.append(asr)
+
         if (clean_accuracy - prune_accuracy) > X[output]:
             file_name = "B_pi_net_" + str(X[output]) + ".h5"
             B_pi_model.save("models/"+file_name)
@@ -75,6 +140,8 @@ def main(X):
             output = output + 1
         if output >= len(X):
             break
+
+    plot(clean_accuracy_set, asr_set)
 
     # while (clean_accuracy-prune_accuracy) < X:
     #     target_channel = np.arg
